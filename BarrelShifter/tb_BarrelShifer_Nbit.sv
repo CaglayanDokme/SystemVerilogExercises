2a77e656e538048705b0dc4fbc7e0bea049b32f8
Caglayan DOKME <caglayandokme@gmail.com>
2021-05-14 17:20:14 +0300

    Direction change added to test scenario

--- BarrelShifter/tb_BarrelShifer_Nbit.sv
+++ BarrelShifter/tb_BarrelShifer_Nbit.sv
@@ -1,4 +1,4 @@
-/* File:            tb_BarrelShifer_Nbit.sv
+/* File:            tb_BarrelShifter_Nbit.sv
  * Author:          Caglayan DOKME, caglayandokme@gmail.com
  * Date:            May 14, 2021 -> Created
  * Description:     Testbench for N-Bit Barrel Shifter module
@@ -6,19 +6,20 @@
 
 `timescale 1ns / 1ps
 
-module tb_BarrelShifer_Nbit;
+module tb_BarrelShifter_Nbit;
 
 // Local Constants
-localparam N    = 32;
+localparam N    = 8;
 localparam logN = $clog2(N);
 
 // Test Signals
 logic [N - 1    : 0] test_a;
 logic [logN - 1 : 0] test_shiftAmount;
+logic                test_direction;
 logic [N - 1    : 0] test_shifted;
 
 // Instantiation of Circuit Under Test
-BarrelShifer_Nbit #(.N(N)) BarrelShifter(.a(test_a), .shiftAmount(test_shiftAmount), .shifted(test_shifted));
+BarrelShifter_Nbit #(.N(N)) BarrelShifter(.a(test_a), .shiftAmount(test_shiftAmount), .direction(test_direction) , .shifted(test_shifted));
 
 // Test Scenario
 initial begin
@@ -29,6 +30,19 @@ initial begin
         $display("Bit size int:     %0d", $bits(int));
     end
 
+    $display("Testing the right shift.");
+    test_direction = 0;
+    for(longint i = 0; i < 2**N; ++i) begin   // Iterate for each N-bit signal
+        for(int k = 0; k < 2**logN; ++k) begin // Iterate for each shift amount(0 to log2N)
+            test_a              = i;
+            test_shiftAmount    = k;
+            
+            #10;
+        end
+    end    
+    
+    $display("Testing the left shift.");
+    test_direction = 1;     // Repeat the test for the opposite direction 
     for(longint i = 0; i < 2**N; ++i) begin   // Iterate for each N-bit signal
         for(int k = 0; k < 2**logN; ++k) begin // Iterate for each shift amount(0 to log2N)
             test_a              = i;
