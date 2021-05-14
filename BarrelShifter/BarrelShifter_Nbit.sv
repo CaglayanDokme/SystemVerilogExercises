7ca17affa1d18c317a1c34a8f35e9e2774c836cc
Caglayan DOKME <caglayandokme@gmail.com>
2021-05-14 17:02:53 +0300

    Direction functionality added.
    File renamed

--- BarrelShifter/BarrelShifter_Nbit.sv
+++ BarrelShifter/BarrelShifter_Nbit.sv
@@ -1,4 +1,4 @@
-/* File:            BarrelShifer_Nbit.sv
+/* File:            BarrelShifter_Nbit.sv
  * Author:          Caglayan DOKME, caglayandokme@gmail.com
  * Date:            May 14, 2021 -> Created
  * Description:     Shifts the given signal to the right according to the given shift amount
@@ -6,31 +6,35 @@
 
 `timescale 1ns / 1ps
 
-module BarrelShifer_Nbit
+module BarrelShifter_Nbit
 #(parameter N = 8)  // N must be an exact power of 2
 (
     input  logic [N-1           : 0] a,
     input  logic [$clog2(N)-1   : 0] shiftAmount,
+    input  logic                     direction,     // 1: left - 0: right 
     output logic [N-1           : 0] shifted
 );
 
 // Internal Signals
-logic [N-1:0] p [N-1:0];
+logic [N-1:0] p_left  [N-1:0];
+logic [N-1:0] p_right [N-1:0];
 
-assign p[0] = a;
+// Body
+assign p_left[0]    = a;
+assign p_right[0]   = a;
 
-/* Normal shift operator doesn't support circular shifts. 
- * So, we need to merge back the cyclic bits. 
- * As the shift direction is to the right, we shall  
- * concatenate them on the most left side of the vector. */
 generate
     genvar i;
     for(i = 1; i < N; ++i) begin
-        assign p[i] = (a >> i) | {(a[i-1 : 0]), {(N - i){1'b0}}};
+        assign p_right[i] = {a[i-1 : 0],  a[N-1 : i]};
+    end
+    
+    for(i = 1; i < N; ++i) begin
+        assign p_left[i] = {a[N-1-i : 0], a[N-1 : N-i]};
     end
 endgenerate
 
-// Body
-assign shifted = p[shiftAmount];
+// Output Logic
+assign shifted = direction ? p_left[shiftAmount] : p_right[shiftAmount]; 
 
 endmodule
